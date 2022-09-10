b75acdcac2bf21d251de9d2dbc3f06d8fc8d842e
prototypa <prototypearea.dev@gmail.com>
2022-09-09 22:36:33 -0400

    Not optimize images for now

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  // images: {
-  //   unoptimized: true,
-  // }
+  images: {
+    unoptimized: true,
+  }
 }
