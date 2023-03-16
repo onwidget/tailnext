089a67b21f860a733e9a9b4dd819eab12f320646
widgeter <onwidget.developer@gmail.com>
2023-03-16 16:32:50 +0100

    Fix first click issue on change theme button

--- src/components/atoms/ToggleDarkMode.tsx
+++ src/components/atoms/ToggleDarkMode.tsx
@@ -6,13 +6,15 @@ import { IconSun, IconMoon } from '@tabler/icons-react';
 
 const ToggleDarkMode = () => {
   const [mounted, setMounted] = useState<boolean>(false);
-  const { theme, setTheme } = useTheme();
+  const { systemTheme, theme, setTheme } = useTheme();
+
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+
+  const handleOnClick = () => setTheme(currentTheme === 'dark' ? 'light' : 'dark');
 
   useEffect(() => {
     setMounted(true);
   }, []);
-
-  const handleOnClick = () => setTheme(theme === 'dark' ? 'light' : 'dark');
 
   return (
     <button
@@ -20,7 +22,15 @@ const ToggleDarkMode = () => {
       className="inline-block rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
       aria-label="Toggle Dark Mode"
     >
-      {mounted && theme === 'dark' ? <IconMoon className="h-5 w-5" /> : <IconSun className="h-5 w-5" />}
+      {mounted ? (
+        currentTheme === 'dark' ? (
+          <IconMoon className="h-5 w-5" />
+        ) : (
+          <IconSun className="h-5 w-5" />
+        )
+      ) : (
+        <div className="h-5 w-5"></div>
+      )}
     </button>
   );
 };
