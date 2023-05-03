4d3af595d9ec7f0556279cb9e16e8c76c72d6747
prototypa <prototypearea.dev@gmail.com>
2023-05-03 18:03:56 -0400

    Update dependencies

--- app/(blog)/blog/head.tsx
+++ app/(blog)/blog/head.tsx
@@ -2,5 +2,5 @@ import { NextSeo } from 'next-seo';
 import { SITE } from '~/config.js';
 
 export default function Head() {
-  return <NextSeo title={`Blog — ${SITE.name}`} description={SITE.description} useAppDir={true} />;
+  return <NextSeo title={`Blog — ${SITE.name}`} description={SITE.description} />;
 }
