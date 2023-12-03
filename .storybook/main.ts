713fdb395e1ac7955aea435d477a42abf459d947
prototypa <prototypearea.dev@gmail.com>
2023-12-03 10:34:47 -0500

    Add @storybook/addon-a11y for Accessibility

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,8 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
-    '@storybook/addon-themes'
+    '@storybook/addon-themes',
+    '@storybook/addon-a11y'
   ],
   framework: {
     name: '@storybook/nextjs',
