f811b554c66b1b1aa09fbe6ffb90c71a51e3e16e
widgeter <onwidget.developer@gmail.com>
2023-12-08 22:33:40 +0100

    feat: modify docs in steps widget with doc blocks

--- src/stories/widgets/Steps.stories.tsx
+++ src/stories/widgets/Steps.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { Title, Subtitle, Description, Primary, Controls, Story, Stories } from '@storybook/blocks';
 
 import Component from '~/components/widgets/Steps';
 import { stepsHome as mockData } from '~/shared/data/pages/home.data';
@@ -10,6 +11,19 @@ const meta = {
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'fullscreen',
+    // Offers several doc blocks to help document your components. More info: https://storybook.js.org/docs/writing-docs/doc-blocks
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <Controls exclude={['id', 'header']} />
+          <Stories includePrimary={false} title={'Stories'} />
+        </>
+      ),
+    },
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
