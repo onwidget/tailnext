24ee27e5c790cc4cb97395625ff344b71764ffa3
widgeter <onwidget.developer@gmail.com>
2023-12-19 10:44:47 +0000

    fix: accessibility on features component
    ARIA hidden element must not be focusable or contain focusable elements

--- src/components/common/WidgetWrapper.tsx
+++ src/components/common/WidgetWrapper.tsx
@@ -4,9 +4,7 @@ import Background from './Background';
 
 const WidgetWrapper = ({ children, id, hasBackground, containerClass }: WrapperTagProps) => (
   <section className="relative not-prose scroll-mt-[72px]" id={id}>
-    <div className="absolute inset-0 pointer-events-none -z-[1]" aria-hidden="true">
-      <Background hasBackground={hasBackground} />
-    </div>
+    <Background hasBackground={hasBackground} />
     <div
       className={twMerge(
         'relative mx-auto max-w-7xl px-4 md:px-6 py-12 md:py-16 lg:py-20 text-default',
