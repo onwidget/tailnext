858234402943103e314e2d3a254b194170d78f2e
Tonia Gonzalez <toniagonzalez.design@gmail.com>
2024-02-24 22:32:52 -0500

    chore: Add logo to header

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image';
+
+import toniaLogo from '~/assets/images/tg_logo.svg';
+
 const Logo = () => (
   <span className="ml-2 self-center whitespace-nowrap text-2xl font-bold text-gray-900 dark:text-white md:text-xl">
-    {/* <Image src="../src/assets/images/tg_logo.svg" width="30" height="30" alt="logo"/> */}
-    txg
+    <Image src={toniaLogo} width="40" height="40" alt="logo"/>
   </span>
 );
 
