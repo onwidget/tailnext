d4ae27770a2d66962cb7ab3de4836f36ab039147
Tonia Gonzalez <toniagonzalez.design@gmail.com>
2024-03-03 22:34:57 -0500

    chore: Change art page hero image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
 const aboutUrl = 'https://images.pexels.com/photos/1118667/pexels-photo-1118667.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
-const artUrl = 'https://images.pexels.com/photos/1570779/pexels-photo-1570779.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
+const artUrl = 'https://images.pexels.com/photos/1154723/pexels-photo-1154723.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
 const codeUrl = 'https://images.pexels.com/photos/785418/pexels-photo-785418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
 const homeUrl = 'https://images.pexels.com/photos/691710/pexels-photo-691710.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
